eb36f51d0bb7df79f18338e6cea08fbdec43c584
Kévin Begot <kevin.begot@MacBook-Air-de-Kevin.local>
2022-12-13 23:18:05 +0100

    [FIX] debug and comment code

--- GameEngineClass.cs
+++ GameEngineClass.cs
@@ -22,9 +22,9 @@ namespace Csharp // Note: actual namespace depends on the project name.
                                     if (playerTurn)
                                     {
                                         Player1Turn(result, board);
-                                        if (CheckWinCondition('X', board))
+                                        if (WinCondition('X', board)) // if X retrun true in WinCondition then the player1 scores 1 point. Displays the score of the players
                                         {
-                                            EndGame("\n" + player1.Prenom + " à gagné et marque 1 point!", board);
+                                            End("\n" + player1.Prenom + " à gagné et marque 1 point!", board);
                                             player1.Score += 1;
                                             Console.WriteLine("\n" + player1.Prenom + ": " + player1.Score + " Poin(s)\n" + player2.Prenom + ": " + player2.Score + " Point(s)");
                                             break;
@@ -33,18 +33,19 @@ namespace Csharp // Note: actual namespace depends on the project name.
                                     else
                                     {
                                         Player2Turn(result, board);
-                                        if (CheckWinCondition('O', board))
+                                        if (WinCondition('O', board)) // if O retrun true in WinCondition then the player2 scores 1 point. Displays the score of the players
                                         {
-                                            EndGame("\n" + player2.Prenom + " à gagné et marque 1 point!", board);
+                                            End("\n" + player2.Prenom + " à gagné et marque 1 point!", board);
                                             player2.Score += 1;
                                             Console.WriteLine("\n" + player1.Prenom + ": " + player1.Score + " Poin(s)\n" + player2.Prenom + ": " + player2.Score + " Point(s)");
                                             break;
                                         }
                                     }
                                     playerTurn = !playerTurn;
-                                    if (CheckFullBoard(board))
+                                    if (FullBoard(board)) // if the FullBoard is true then equality. Displays the score of the players
                                     {
-                                        EndGame("égalité", board);
+                                        End("égalité", board);
+                                        Console.WriteLine("\n" + player1.Prenom + ": " + player1.Score + " Poin(s)\n" + player2.Prenom + ": " + player2.Score + " Point(s)");
                                         break;
                                     }
                                 }
@@ -52,14 +53,14 @@ namespace Csharp // Note: actual namespace depends on the project name.
                             
                                 if (!result)
                                 {
-                                    Console.WriteLine("\n Rejouer [enter], ou quitter [escape]?");
+                                    Console.WriteLine("\nRejouer [enter] | Quitter [Q]");
                                 GetInput:
                                     switch (Console.ReadKey(true).Key)
                                     {
-                                        case ConsoleKey.Enter: break;
-                                        case ConsoleKey.Escape:
-                                            result = true;
+                                        case ConsoleKey.Enter: break; // Can play again
+                                        case ConsoleKey.Q: // Quit app
                                             Console.Clear();
+                                            Environment.Exit(0);
                                             break;
                                         default: goto GetInput;
                                     }
@@ -67,12 +68,10 @@ namespace Csharp // Note: actual namespace depends on the project name.
                             }
         }
 
-        private static bool CheckFullBoard(char[,] board)
+        private static bool FullBoard(char[,] board) // check if the table is complete and return true or false
         {
             if(
-            board[0, 0] != ' ' && board[1, 0] != ' ' && board[2, 0] != ' ' &&
-            board[0, 2] != ' ' && board[1, 2] != ' ' && board[2, 2] != ' ' &&
-            board[0, 1] != ' ' && board[1, 1] != ' ' && board[2, 1] != ' ')
+            board[0, 0] != ' ' && board[1, 0] != ' ' && board[2, 0] != ' ' && board[0, 2] != ' ' && board[1, 2] != ' ' && board[2, 2] != ' ' && board[0, 1] != ' ' && board[1, 1] != ' ' && board[2, 1] != ' ')
             {
                 return true;
             } else {
@@ -81,7 +80,7 @@ namespace Csharp // Note: actual namespace depends on the project name.
         }
 
         
-        private static void Player2Turn(bool result, char[,] board)
+        private static void Player2Turn(bool result, char[,] board) // Management of player 1's turn
         {
             var (row, column) = (0, 0);
                 bool move = false;
@@ -89,17 +88,17 @@ namespace Csharp // Note: actual namespace depends on the project name.
                 {
                     Console.Clear();
                     Board(board);
-                    Console.SetCursorPosition(column * 6 + 1, row * 4 + 1);
+                    Console.SetCursorPosition(column * 6 + 1, row * 4 + 1); // Set cursor position
                     switch (Console.ReadKey(true).Key)
                     {
-                        case ConsoleKey.UpArrow: row = row <= 0 ? 2 : row - 1; break;
+                        case ConsoleKey.UpArrow: row = row <= 0 ? 2 : row - 1; break; // Move the cursor with the arrows to be able to put its cross
                         case ConsoleKey.DownArrow: row = row >= 2 ? 0 : row + 1; break;
                         case ConsoleKey.LeftArrow: column = column <= 0 ? 2 : column - 1; break;
                         case ConsoleKey.RightArrow: column = column >= 2 ? 0 : column + 1; break;
                         case ConsoleKey.Enter:
-                            if (board[row, column] is ' ')
+                            if (board[row, column] is ' ') // Fill the table if it is empty
                             {
-                                board[row, column] = 'O';
+                                board[row, column] = 'O'; 
                                 move = true;
                             }
                             break;
@@ -111,14 +110,14 @@ namespace Csharp // Note: actual namespace depends on the project name.
                 }
         }
 
-        private static void EndGame(object value,char[,] board)
+        private static void End(object value,char[,] board) // Display a message
         {
             Console.Clear();
             Board(board);
             Console.WriteLine();
             Console.Write(value);
         }
-        private static void Board(char[,] board)
+        private static void Board(char[,] board) // Initialization and display of the table
         {
             Console.WriteLine($" {board[0, 0]}  ║  {board[0, 1]}  ║  {board[0, 2]}");
             Console.WriteLine("    ║     ║");
@@ -130,17 +129,13 @@ namespace Csharp // Note: actual namespace depends on the project name.
             Console.WriteLine("    ║     ║");
             Console.WriteLine($" {board[2, 0]}  ║  {board[2, 1]}  ║  {board[2, 2]}");
         }
-        private static bool CheckWinCondition(char k, char[,] board)
+        private static bool WinCondition(char k, char[,] board) // check if there is a win condition and return true or false
         {
             if(
-            board[0, 0] == k && board[1, 0] == k && board[2, 0] == k ||
-            board[0, 1] == k && board[1, 1] == k && board[2, 1] == k ||
-            board[0, 2] == k && board[1, 2] == k && board[2, 2] == k ||
-            board[0, 0] == k && board[0, 1] == k && board[0, 2] == k ||
-            board[1, 0] == k && board[1, 1] == k && board[1, 2] == k ||
-            board[2, 0] == k && board[2, 1] == k && board[2, 2] == k ||
-            board[0, 0] == k && board[1, 1] == k && board[2, 2] == k ||
-            board[2, 0] == k && board[1, 1] == k && board[0, 2] == k)
+            board[0, 0] == k && board[1, 0] == k && board[2, 0] == k ||board[0, 1] == k && board[1, 1] == k && board[2, 1] == k ||
+            board[0, 2] == k && board[1, 2] == k && board[2, 2] == k ||board[0, 0] == k && board[0, 1] == k && board[0, 2] == k ||
+            board[1, 0] == k && board[1, 1] == k && board[1, 2] == k ||board[2, 0] == k && board[2, 1] == k && board[2, 2] == k ||
+            board[0, 0] == k && board[1, 1] == k && board[2, 2] == k ||board[2, 0] == k && board[1, 1] == k && board[0, 2] == k)
             {
                 return true;
             } else{
@@ -148,7 +143,7 @@ namespace Csharp // Note: actual namespace depends on the project name.
             }
         }
 
-        private static void Player1Turn(bool result, char[,] board)
+        private static void Player1Turn(bool result, char[,] board) // Management of player 2's turn
         {
             var (row, column) = (0, 0);
                 bool move = false;
@@ -157,17 +152,17 @@ namespace Csharp // Note: actual namespace depends on the project name.
                     Console.Clear();
                     Board(board);
 
-                    Console.SetCursorPosition(column * 6 + 1, row * 4 + 1);
+                    Console.SetCursorPosition(column * 6 + 1, row * 4 + 1); // Set cursor position
                     switch (Console.ReadKey(true).Key)
                     {
-                        case ConsoleKey.UpArrow: row = row <= 0 ? 2 : row - 1; break;
+                        case ConsoleKey.UpArrow: row = row <= 0 ? 2 : row - 1; break; // Move the cursor with the arrows to be able to put its cross
                         case ConsoleKey.DownArrow: row = row >= 2 ? 0 : row + 1; break;
                         case ConsoleKey.LeftArrow: column = column <= 0 ? 2 : column - 1; break;
                         case ConsoleKey.RightArrow: column = column >= 2 ? 0 : column + 1; break;
                         case ConsoleKey.Enter:
                             if (board[row, column] is ' ')
                             {
-                                board[row, column] = 'X';
+                                board[row, column] = 'X'; // Fill the table if it is empty
                                 move = true;
                             }
                             break;
